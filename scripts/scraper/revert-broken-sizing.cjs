26d676089bfd98797f7ea85ed247a4fe5122ce8c
subhansaim047 <subhansaim047@gmail.com>
2026-09-13 01:07:42 +0300

    fix: revert broken sizing changes and restore stable production state

--- scripts/scraper/revert-broken-sizing.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
-const html = fs.readFileSync('public/demos/dilicious-pizza/index.html', 'utf8');
-const $ = cheerio.load(html, { decodeEntities: false });
-
-$('#global-section-sizing-fix').remove();
-
-fs.writeFileSync('public/demos/dilicious-pizza/index.html', $.html());
-console.log('Successfully removed #global-section-sizing-fix');
