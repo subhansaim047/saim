c6d41706739482815b7ecd16e425dbf34d6c3bd5
subhansaim047 <subhansaim047@gmail.com>
2026-09-13 15:30:55 +0300

    fix: restore contact page link, update header phone, and fix menu layout css/wrapper

--- check.cjs
+++ check.cjs
@@ -3,7 +3,5 @@ const cheerio = require('cheerio');
 const indexHtml = fs.readFileSync('public/demos/dilicious-pizza/index.html', 'utf8');
 const $ = cheerio.load(indexHtml, { decodeEntities: false });
 
-console.log("Top level sections:");
-$('div.elementor-top-section, section.elementor-top-section').each((i, el) => {
-    console.log(el.tagName + " " + $(el).attr('class'));
-});
+console.log("Header HTML:");
+console.log($('.site-header').html().substring(2000, 4000));
